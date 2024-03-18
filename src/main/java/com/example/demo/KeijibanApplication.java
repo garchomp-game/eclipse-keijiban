c64149218acf2a90614e239c2660b16f234c4732
garchomp-game <otoka.japanesedrum@gmail.com>
2024-03-18 14:55:31 +0900

    commit

--- src/main/java/com/example/demo/KeijibanApplication.java
+++ src/main/java/com/example/demo/KeijibanApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KeijibanApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(KeijibanApplication.class, args);
 	}
-
 }
